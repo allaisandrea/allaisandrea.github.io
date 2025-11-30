c255383bfbcb7563f44c576c6398f58682507090
andrea <allais.andrea@gmail.com>
2025-11-30 13:22:03 -0800

    Added graphical model

---
+++ _plot/reinforcement_learning.py
@@ -0,0 +1,45 @@
+from typing import Callable, Sequence
+import sys
+import abc
+import os
+import dataclasses
+import matplotlib.collections
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import matplotlib
+from matplotlib.path import Path
+import lib
+
+
+def plot_graphical_model(output_path: str):
+    spacing_x = 80
+    s_nodes = [
+        lib.Node((spacing_x * i, 60), text, 17)
+        for i, text in enumerate([r"$s_0$", r"$s_1$", r"$s_2$", r"$s_3$", ""])
+    ]
+    a_nodes = [
+        lib.Node((spacing_x * (i + 0.5), 0), text, 17)
+        for i, text in enumerate([r"$a_0$", r"$a_1$", r"$a_2$", ""])
+    ]
+
+    figure, axes = lib.make_graph_figure(np.array([[-20, -20], [310, 80]]))
+    for node in s_nodes[:-1] + a_nodes[:-1]:
+        lib.plot_node(axes, node)
+    for node1, node2 in zip(s_nodes[:-1], s_nodes[1:]):
+        lib.plot_edge(axes, node1, node2)
+    for node1, node2 in zip(s_nodes[:-1], a_nodes):
+        lib.plot_edge(axes, node1, node2)
+    for node1, node2 in zip(a_nodes[:-1], s_nodes[1:-1]):
+        lib.plot_edge(axes, node1, node2)
+    figure.savefig(os.path.join(output_path, "graphical_model.svg"))
+
+
+if __name__ == "__main__":
+    task = sys.argv[1]
+    output_path = os.path.join(lib.pages_dir(), "reinforcement_learning")
+    match task:
+        case "graphical_model":
+            plot_graphical_model(output_path)
+        case _:
+            raise ValueError(f"Unknown task: {task}")
---
+++ pages/reinforcement_learning/index.md
@@ -0,0 +1,10 @@
+---
+layout: default
+title: Reinforcement learning
+---
+
+# Reinforcement learning
+
+<figure>
+<img src="graphical_model.svg" alt="Graphical model" style="max-width:5in"/>
+</figure>
